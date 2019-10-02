807de255ebe95d071726768f91b6109d24b8bf9c
shukanov <namra2014@gmail.com>
2019-10-02 18:35:11 +0400

    release v1

---
+++ profile.php
@@ -0,0 +1,122 @@
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="widtd=device-widtd, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="autdor" content="">
+
+    <title>test</title>
+	
+	<link href="style.css" rel="stylesheet">
+	<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+	<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  </head>
+  <body>
+      <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+        <div class="container">
+		<a class="navbar-brand" href="#">Заря</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="/">Главная <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="event.php">Секции</a>
+            </li>
+			<li class="nav-item active">
+              <a class="nav-link" href="profile.php">Личный кабинет</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled" href="#">О нас</a>
+            </li>
+          </ul>
+          <form class="form-inline mt-2 mt-md-0">
+            <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+          </form>
+        </div>
+		</div>
+      </nav>
+
+	<div class="wrap">
+		<div class="wrapper">
+			<div class="container emp-profile">
+				<form metdod="post">
+					<div class="row">
+						<div class="col-md-4">
+							<div class="profile-img mb-3" style="background-image:url('images/profile.jpg')"></div>
+							<div class="profile-work">
+								<div class="profile-children mb-3">
+									<p class="lead">Дети</p>
+									<hr>
+									<p>Егорова Елена<br>Егоров Степан</p>
+								</div>
+								
+								<div class="profile-hasben mb-3">								
+									<p class="lead">Муж</p>
+									<hr>
+									<p>Егоров Иван<br>Тел: +7(999)224-95-02<br>Адрес: г. Волгоград<br>ул. Мира 52 кв.31</p>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-8">
+							<div class="profile-head">
+										<h5>
+											Егорова Татьяна Геннадьевна
+										</h5>
+										<p class="lead">Личная и контактная информация</p>
+										<table class="table table-bordered table-hover">
+
+										  <tbody>
+											<tr>
+											  <td scope="row">Профессия</td>
+											  <td>Врач больницы №25</td>
+											  
+											</tr>
+											<tr>
+											  <td scope="row">Дата рождения</td>
+											  <td>01.01.1984</td>
+											  
+											</tr>
+											<tr>
+											  <td scope="row">Адрес</td>
+											  <td>г. Волгоград ул. Мира 52 кв.31</td>
+											
+											</tr>
+											<tr>
+											  <td scope="row">Телефон</td>
+											  <td>+7(992)421-42-53</td>
+											 
+											</tr>
+											<tr>
+											  <td scope="row">Эл.почта</td>
+											  <td>kshitighelani@gmail.com</td>
+											</tr>
+											<tr>
+											  <td scope="row">Адрес работы</td>
+											  <td>Волгоград ул. Ленина 5</td>
+											</tr>
+										  </tbody>
+										</table>
+									
+							</div>
+						</div>
+					</div>
+
+				</form>           
+			</div>
+		</div>
+	</div>
+		
+    <footer class="container">
+		<hr>
+        <p>&copy; 2019 Company, Inc. </p>
+      </footer>
+  </body>
+</html>
